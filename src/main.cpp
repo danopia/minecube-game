7e85bfc3864147e5fe6b0e9813b50a680c272fc1
Liam Dunn <liam.dunn144@gmail.com>
2010-12-30 23:26:54 -0200

    Somewhat screwy addition to makeTerrain. I'm failing somewhere

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,33 @@
 
 using namespace std;
 
-Octree<bool> makeTerrain()
+Octree<bool> makeTerrain(int level)
 {
+    cout << "At level " << level << endl;
     vector<Octree<bool> >blocks;
     for(int i = 0; i < 8; i++)
     {
-        bool type = (sf::Randomizer::Random(-1.f, 1.f) <= 0.0f ? true : false);
-        blocks.push_back(Octree<bool>(type));
-        cout << "Block " << i << ": " << (type ? "dirt" : "air") << endl;
+        bool leaf = (sf::Randomizer::Random(-1.f, 1.f) <= 0.0f ? true : false);
+        if(level < 2)
+        {
+
+            if(leaf)
+            {
+                bool type = (sf::Randomizer::Random(-1.f, 1.f) <= 0.0f ? true : false);
+                blocks.push_back(Octree<bool>(type));
+            } 
+            else
+            {
+                cout << "Going deeper!" << endl;
+                blocks.push_back(makeTerrain(level + 1));
+            }
+        }
+        else if(level == 2)
+        {
+            bool type = (sf::Randomizer::Random(-1.f, 1.f) <= 0.0f ? true : false);
+            blocks.push_back(Octree<bool>(type));
+        }
+        cout << "Block " << i << "at level " << level << ": " << (leaf ? "leaf" : "node") << endl;
     }
     Octree<bool> terrain(blocks);
     return terrain;
@@ -38,7 +57,7 @@ int main()
     float Left = 0.f;
     float Top  = 0.f;
 
-    Octree<bool> terrain = makeTerrain();
+    Octree<bool> terrain = makeTerrain(0);
     
     // Start game loop
     while (App.IsOpened())
