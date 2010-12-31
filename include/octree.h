92c262a14010e145547c3798fc661a8c1093b47a
Liam Dunn <liam.dunn144@gmail.com>
2010-12-31 02:25:32 -0200

    Move octree functions out of class definition

--- include/octree.h
+++ include/octree.h
@@ -13,48 +13,62 @@ template <class T>
 class Octree
 {
     public:
-        Octree()
-        {
-            value = 0;
-            hasChildren = false; //Default to a leaf
-        }
-        Octree(vector<Octree<T> > initchildren)
-        {
-            value = 0;
-            hasChildren = true;
-            children = initchildren;
-        }
-        Octree(T initvalue)
-        {
-            value = initvalue;
-            hasChildren = false;
-            children = vector<Octree<T> >(0);
-        }
-
         //Only value or children will ever be non-null
         T value; 
         vector<Octree<T> > children;
         bool hasChildren;
 
-        bool Insert(T val)
-        {
-            if(!hasChildren)
-                return false;
-            //Go to the next free cell
-            int freeIndex = 0;
-            while(children[freeIndex] != NULL && freeIndex <= 7)
-                freeIndex++;
-            if(children[freeIndex] != NULL) return false;
-            children[freeIndex] = val;
-            return true;
-        }   
-        bool Remove(int index)
-        {
-            if(!hasChildren || children[index] == NULL)
-                return false;
-            children[index] = NULL;
-            return true;
-        }
+        Octree();
+        Octree(vector<Octree<T> > initchildren);
+        Octree(T initvalue);
+        bool Insert(T val);
+        bool Remove(int index);
 
 };
+
+template <class T>
+Octree<T>::Octree()
+{
+    value = 0;
+    hasChildren = false; //Default to a leaf
+}
+
+template <class T>
+Octree<T>::Octree(vector<Octree<T> > initchildren)
+{
+    value = 0;
+    hasChildren = true;
+    children = initchildren;
+}
+
+template <class T>
+Octree<T>::Octree(T initvalue)
+{
+    value = initvalue;
+    hasChildren = false;
+    children = vector<Octree<T> >(0);
+}
+
+template <class T>
+bool Octree<T>::Insert(T val)
+{
+    if(!hasChildren)
+        return false;
+    //Go to the next free cell
+    int freeIndex = 0;
+    while(children[freeIndex] != NULL && freeIndex <= 7)
+        freeIndex++;
+    if(children[freeIndex] != NULL) return false;
+        children[freeIndex] = val;
+    return true;
+}   
+
+template <class T>
+bool Octree<T>::Remove(int index)
+{
+    if(!hasChildren || children[index] == NULL)
+        return false;
+    children[index] = NULL;
+    return true;
+}
 #endif
