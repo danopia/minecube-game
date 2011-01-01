2ef73a38fac4f0d64983fdc6740f015ef34875a4
Daniel Lamando <me@danopia.net>
2011-01-01 00:44:14 -0500

    Add some color

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -4,85 +4,97 @@
 Renderer::Renderer(Terrain initterrain) : terrain(initterrain) {
     // Set color and depth clear value
     glClearDepth(1.f);
-    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClearColor(0.f, 0.f, 0.f, 1.f); // black
 
     // Enable Z-buffer read and write
     glEnable(GL_DEPTH_TEST);
     glDepthMask(GL_TRUE);
 
-
+    // Face culling to render half the polys
     glEnable(GL_CULL_FACE);
+    
     // Setup a perspective projection
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(90.f, 1.f, 1.f, 500.f);
 
-
-    // Lighting test junk
-    GLfloat LightAmbient[]= { 0.5f, 0.5f, 0.5f, 1.0f }; 				// Ambient Light Values ( NEW )
-    GLfloat LightDiffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };				 // Diffuse Light Values ( NEW )
-    GLfloat LightPosition[]= { 0.0f, 0.0f, 2.0f, 1.0f };				 // Light Position ( NEW )
-    glShadeModel(GL_SMOOTH);						// Enable Smooth Shading
+    // Lighting
+    GLfloat LightAmbient[]  = { 0.5f, 0.5f, 0.5f, 1.0f }; // Ambient Light Values
+    GLfloat LightDiffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };	// Diffuse Light Values
+    GLfloat LightPosition[] = { 0.0f, 0.0f, 2.0f, 1.0f };	// Light Position
     glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);				// Setup The Ambient Light
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);				// Setup The Diffuse Light
     glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);			// Position The Light
     glEnable(GL_LIGHT1);							// Enable Light One
-    glEnable(GL_LIGHTING);
+    
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE); // Let glcolor work with lighting
+    glEnable(GL_COLOR_MATERIAL); // Enable lighting
+    glEnable(GL_LIGHTING); // Enable lighting
+    
+    glShadeModel(GL_SMOOTH); // Enable Smooth Shading
 }
 
 void drawCube(float x, float y, float z, float length) {
     float sublength = length / 2;
 
+	  glPushMatrix(); // Preserve world matrix
+	  
     // Apply some transformations
-	  glPushMatrix();
     glTranslatef(x + sublength, y + sublength, z + sublength);
     glScalef(sublength, sublength, sublength);
 
     glBegin(GL_QUADS);
-        float randred = sf::Randomizer::Random(0.0f, 1.0f);
-        float randblue = sf::Randomizer::Random(0.0f, 1.0f);
-        float randgreen = sf::Randomizer::Random(0.0f, 1.0f);
 
-        glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(0.54f, 0.27f, 0.07f); // Dirt
+
+    glColor3f(0.27f, 0.54f, 0.f); // Grass
+    
 		// Front Face
 		glNormal3f( 0.0f, 0.0f, 1.0f);					// Normal Pointing Towards Viewer
-		/*glTexCoord2f(0.0f, 0.0f);*/ glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 1 (Front)
-		/*glTexCoord2f(1.0f, 0.0f);*/ glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 2 (Front)
-		/*glTexCoord2f(1.0f, 1.0f);*/ glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Front)
-		/*glTexCoord2f(0.0f, 1.0f);*/ glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 4 (Front)
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 1 (Front)
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 2 (Front)
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Front)
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 4 (Front)
+
+    glColor3f(0.54f, 0.27f, 0.07f); // Dirt
+		
 		// Back Face
 		glNormal3f( 0.0f, 0.0f,-1.0f);					// Normal Pointing Away From Viewer
 		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Back)
 		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 2 (Back)
 		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 3 (Back)
 		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 4 (Back)
+		
 		// Top Face
 		glNormal3f( 0.0f, 1.0f, 0.0f);					// Normal Pointing Up
 		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 1 (Top)
 		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 2 (Top)
 		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Top)
 		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 4 (Top)
+		
 		// Bottom Face
 		glNormal3f( 0.0f,-1.0f, 0.0f);					// Normal Pointing Down
 		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Bottom)
 		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 2 (Bottom)
 		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 3 (Bottom)
 		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 4 (Bottom)
+		
 		// Right face
 		glNormal3f( 1.0f, 0.0f, 0.0f);					// Normal Pointing Right
 		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Point 1 (Right)
 		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Point 2 (Right)
 		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Point 3 (Right)
 		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Point 4 (Right)
+		
 		// Left Face
 		glNormal3f(-1.0f, 0.0f, 0.0f);					// Normal Pointing Left
 		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Point 1 (Left)
 		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Point 2 (Left)
 		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Point 3 (Left)
 		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Point 4 (Left)
-	glEnd();								// Done Drawing Quads
-	
-	  glPopMatrix();
+		
+	  glEnd(); // Done Drawing Quads
+	  glPopMatrix(); // Undo the translations
 }
 
 void renderNode(Octree<bool> terrain, float x, float y, float z, float size) {
