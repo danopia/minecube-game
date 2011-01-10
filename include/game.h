9bc4e50a8140f2e762b1b8cb846083226b9034c8
Daniel Lamando <me@danopia.net>
2011-01-09 21:49:33 -0500

    Might need those.

---
+++ include/game.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
+#include <cstdio>
+
+#include "renderer.h"
+#include "input.h"
+#include "octree.h"
+#include "player.h"
+#include "terrain.h"
+
+#ifdef _WIN32
+#define snprintf sprintf_s
+#endif
+
+class Game
+{
+    public:
+        Game(sf::RenderWindow *app);
+        void Loop();
+
+        sf::RenderWindow *app;
+        Player player;
+        Terrain terrain;
+        Renderer renderer;
+        InputHandler inputHandler;
+};
+
+#endif
