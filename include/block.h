80af011fde4d87b560ad572de6379a2fe815b0e7
Daniel Lamando <me@danopia.net>
2011-01-11 16:41:16 -0500

    Here, have some better OOP.

--- include/block.h
+++ include/block.h
@@ -5,79 +5,57 @@
 class Block
 {
     public:
-        Block() {};
+        Block(char Type = 0, int DamageTaken = 0, int DamageThreshold = 0) : Type(Type), faces(0x3F), DamageTaken(DamageTaken), DamageThreshold(DamageThreshold) {};
         virtual Block *newBlock() {};
         
-        char faces; // Faces to render (using 6 bits)
         char Type;
+        char faces;
         int DamageTaken;
         int DamageThreshold;
 };
 
 class AirBlock: public Block
 {
-public:
-    AirBlock() : DamageTaken(0), faces(0x3F), Type(0), DamageThreshold(-1) {};
+    public:
+        AirBlock() : Block(0) {};
 
-    AirBlock *newBlock()
-    {
-        return new AirBlock();
-    }
-
-    char faces;
-    int DamageTaken;
-    char Type;
-    int DamageThreshold;
+        AirBlock *newBlock()
+        {
+            return new AirBlock();
+        }
 };
 
 class StoneBlock: public Block
 {
     public:
-
-        StoneBlock() : DamageTaken(0), faces(0x3F), Type(1), DamageThreshold(50) {};
+        StoneBlock() : Block(1, 0, 50) {};
 
         StoneBlock *newBlock()
         {
             return new StoneBlock();
         }
-        
-        char faces;
-        char Type;
-        int DamageTaken;
-        int DamageThreshold;
 };
 
 class DirtBlock: public Block
 {
     public:
-
-        DirtBlock() : DamageTaken(0), faces(0x3F), Type(2), DamageThreshold(10) {};
+        DirtBlock() : Block(2, 0, 10) {};
 
         DirtBlock *newBlock()
         {
             return new DirtBlock();
         }
-        
-        char faces;
-        char Type;
-        int DamageTaken;
-        int DamageThreshold;
 };
 
 class GrassBlock: public Block
 {
     public:
-
-        GrassBlock() : DamageTaken(0), faces(0x3F), Type(3), DamageThreshold(15) {};
+        GrassBlock() : Block(3, 0, 15) {};
 
         GrassBlock *newBlock()
         {
             return new GrassBlock();
         }
-        
-        char faces;
-        char Type;
-        int DamageTaken;
-        int DamageThreshold;
 };
+
 #endif
