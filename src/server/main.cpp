2462bae0aaf1cfcfcac383a26a926c90b10a1d29
Daniel Lamando <me@danopia.net>
2011-01-13 18:20:01 -0500

    Send heartbeat on startup. Heartbeats are definately going in their own class, don't freak out on me again...

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "common/terrain.h"
+#include "common/player.h"
 
 // Choose a random port for opening sockets (ports < 1024 are reserved)
 const unsigned short Port = 28997;
@@ -27,8 +28,67 @@ void sendTerrain(sf::SocketTCP Client) {
     delete[] memblock;
 }
 
+
+void saveKey(std::string key) {
+    std::ofstream file("key.txt");
+    file.write(key.c_str(), key.length());
+    file.close();
+}
+
+// TODO: error handling
+std::string readKey() {
+    std::ifstream file("key.txt");
+
+    if (!file) return ""; // No key is saved
+    
+    char memblock[33];
+    file.read(memblock, 32);
+    file.close();
+
+    return std::string(memblock);
+}
+
+sf::Http Http;
+void sendBeat() {
+    Http.SetHost("mc-heartbeat.heroku.com");
+
+    // Prepare a request to beat the heart
+    sf::Http::Request Request;
+    Request.SetMethod(sf::Http::Request::Post);
+    Request.SetBody("hey=thar");//"port=" << Port);
+    Request.SetHttpVersion(1, 0);
+    
+    // Load the key (if any)
+    std::string key = readKey();
+    std::cout << "Key: " << key << std::endl;
+    
+    if (key == "")
+        Request.SetURI("/beat");
+    else
+        Request.SetURI("/beat/" + key);
+
+    // Send it and get the response returned by the server
+    sf::Http::Response Page = Http.SendRequest(Request);
+
+    if (Page.GetStatus() != 200) {
+        std::cout << "Error while beating the heart. (HTTP " << Page.GetStatus() << ")" << std::endl;
+        return;
+    }
+
+    // Display the response
+    std::cout << "The heart has been beat." << std::endl;
+    
+    // Save the key
+    saveKey(Page.GetBody());
+}
+
+std::map<sf::SocketTCP, Player> players();
+
 // Launch a server and receive incoming messages
 int main() {
+    std::cout << "Beating..." << std::endl;
+    sendBeat();
+    
     std::cout << "Setting up terrain..." << std::endl;
     terrain = new Terrain(5, 0, 1,1,1, 50);
     terrain->Regenerate();
